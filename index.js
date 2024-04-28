076d0805e639f8fe8b753f79b0c5835524be36ba
codemob <69110900+commandblox@users.noreply.github.com>
2024-04-28 11:55:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const input = document.getElementById("input");
+    const output = document.getElementById("output");
+
+    const base = 10;
+    const max_values = 25;
+
+    input.addEventListener("beforeinput", event=>{
+        if (event.data == null) return;
+        let i = event.data.length;
+        while (i--) {
+            if (!"1234567890".includes(event.data.charAt(i))) {
+                event.preventDefault();
+                return;
+            }
+        }
+    });
+
+    function happyNumberCheck() {
+        output.innerHTML = "";
+        let num = +input.value;
+        let originalNum = num;
+        let i = 0;
+        while (num != 1 && num != 4 && num != 0) {
+            let sum = 0;
+
+            let values = [];
+            while (num > 0) {
+                let rem = num%base;
+                values.unshift(`${rem}<sup>2</sup>`);
+
+                num = Math.floor(num / base);
+                sum += rem*rem;
+            }
+            num = sum;
+            output.innerHTML += `${values.join("&nbsp;+&nbsp;")}&nbsp;=&nbsp;${num}<br>`;
+            if (i >= max_values) {
+                output.innerHTML += `...<br>`
+                break;
+            }
+            i ++;
+        }
+
+        let isHappy = num == 1;
+        output.innerHTML = `<span id="is-happy-${isHappy}">${originalNum}</span><br><br><span class="math">${output.innerHTML}</span>`
+    }
+
+    happyNumberCheck();
+
+    input.addEventListener("input", event=>{
+        if (!input.value) {
+            input.value = "0";
+        }
+        input.value = Number.parseInt(input.value);
+        happyNumberCheck();
+    })
+});
